7655da69d63573a961bf48cb27d24b91310fa8e4
Alex Rummel <alex.rummel.vt@gmail.com>
2021-01-03 23:15:10 -0500

    Add UI elements to menu

--- app/src/main/java/android/example/exploringlayouts/MainActivity.java
+++ app/src/main/java/android/example/exploringlayouts/MainActivity.java
@@ -3,6 +3,7 @@ package android.example.exploringlayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+    }
+
+    void loadConstraintLayout(View v){
+        setContentView(R.layout.activity_main);
+    }
+
+    void loadTableLayout(View v){
+//        setContentView(R.layout.my_table_layout);
     }
 }
